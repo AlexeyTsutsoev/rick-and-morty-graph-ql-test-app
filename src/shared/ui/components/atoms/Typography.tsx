f46a3fb8db9aabc4b1cf7944eed75ba30ea8c577
Alexey Tsutsoev <alexeytsuts@gmail.com>
2023-04-21 21:56:48 +0200

    feat(shared ui): added base components

---
+++ src/shared/ui/components/atoms/Typography.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { TColorKeys, TFontVariant, getColor, getFontSize, getFontWeight } from "../../theme";
+import { StyleSheet, Text, TextProps } from "react-native";
+
+interface TypographyProps extends TextProps {
+  /**
+   * variant for set up `fontWeight` and `fontSize`
+   * default value -> `bodyLarge`
+   */
+  variant?: TFontVariant;
+  /**
+   * key of color
+   * default value -> `content`
+   */
+  color?: TColorKeys;
+}
+
+/**
+ * Wrapper on default Text component from React Native.
+ */
+export const Typography: React.FC<TypographyProps> = ({
+  variant = "bodyLarge",
+  color = "content",
+  style,
+  children,
+  ...rest
+}) => {
+  const styles = React.useMemo(() => {
+    return getStyles(variant, color);
+  }, [variant, color]);
+
+  return (
+    <Text style={[styles.textStyle, style]} {...rest}>
+      {children}
+    </Text>
+  );
+};
+
+const getStyles = (variant: TFontVariant, color: TColorKeys) => {
+  return StyleSheet.create({
+    textStyle: {
+      fontSize: getFontSize(variant),
+      fontWeight: getFontWeight(variant),
+      color: getColor(color),
+    },
+  });
+};
